eb922dcd74f046719b224aa4dfbaa137642b8f54
Phrateus <benjamin.adam22@gmail.com>
2016-02-27 00:19:29 +0100

    Add piece of code to make technical proof of concept.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,109 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QPdfWriter>
+#include <QPainter>
+#include <QPagedPaintDevice>
+
+#include <QtWidgets>
+
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->plainTextEdit->clear();
+
+
+    ///* Put combo boxes in each cell of a QTableWidget
+    ui->tableWidget->setColumnCount(10);
+    ui->tableWidget->setRowCount(10);
+
+    QLabel *label;
+    QComboBox *comboBox;
+
+    for(int i = 0; i < ui->tableWidget->columnCount(); i++)
+    {
+        for(int j = 0; j < ui->tableWidget->rowCount(); j++)
+        {
+            ///* QTableWidget with QComboBox
+            comboBox = new QComboBox(ui->tableWidget);
+            comboBox->addItem("Black");
+            comboBox->addItem("Red");
+            comboBox->setItemData( 0, QColor( Qt::black ), Qt::DecorationRole );
+            comboBox->setItemData( 1, QColor( Qt::red ), Qt::DecorationRole );
+            ui->tableWidget->setCellWidget(i, j, comboBox);
+            //*/
+
+            /* QTableWidget with QLabel
+            label = new QLabel(ui->tableWidget);
+            label->setStyleSheet("QLabel { background-color : red; color : blue; }");
+            ui->tableWidget->setCellWidget(i, j, label);
+            */
+        }
+    }
+    //*/
+
+
+    ///* Write QTableWidget in PDF file.
+    QPdfWriter test("C:/Users/benja/Desktop/test.pdf");
+    test.setPageSize(QPagedPaintDevice::A4);
+    const qreal horizontalMarginMM = 10.0;     // 10 mm margin on each side
+    const qreal verticalMarginMM = 10.0;
+    QPagedPaintDevice::Margins margins;
+    margins.left = margins.right = horizontalMarginMM;
+    margins.top = margins.bottom = verticalMarginMM;
+    test.setMargins(margins);
+
+    //Render the QTableWidget in a QPixmap
+    //QPixmap pixmap(ui->tableWidget->size());
+    QPixmap pixmap(QSize(1920,1080));
+    ui->tableWidget->render(&pixmap);
+
+    //Rotate the QPixmap
+    QTransform t;
+    pixmap = pixmap.transformed(t.rotate(90),Qt::SmoothTransformation);
+
+
+    //Draw the QPixmap with a QPainter
+    QPainter painter;
+    painter.begin(&test);
+    painter.drawPixmap(QRectF(0, 0, test.width(), test.height()), pixmap, QRectF(0, 0,pixmap.width(), pixmap.height()));
+    painter.end();
+
+    ui->plainTextEdit->appendPlainText("PDF creation done.");
+    //*/
+
+
+
+    /* Write text in PDF file.
+    QPdfWriter test("C:/Users/benja/Desktop/test.pdf");
+    test.setPageSize(QPagedPaintDevice::A4);
+    const qreal horizontalMarginMM = 10.0;     // 10 mm margin on each side
+    const qreal verticalMarginMM = 10.0;
+    QPagedPaintDevice::Margins margins;
+    margins.left = margins.right = horizontalMarginMM;
+    margins.top = margins.bottom = verticalMarginMM;
+    test.setMargins(margins);
+
+    QPainter painter;
+    painter.begin(&test);
+
+    painter.setPen(Qt::red);
+    painter.drawText(0,0,"THIS IS MY RED PDF TEXT..");
+
+    painter.setPen(Qt::green);
+    painter.drawText(0,500,"THIS IS MY GREEN PDF TEXT..");
+
+    painter.setPen(Qt::blue);
+    painter.drawText(0,1000,"THIS IS MY BLUE PDF TEXT..");
+
+    painter.end();
+
+    ui->plainTextEdit->appendPlainText("PDF creation done.");
+    */
 }
 
 MainWindow::~MainWindow()
